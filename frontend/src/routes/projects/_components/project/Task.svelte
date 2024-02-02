f6511c01674181fd519d7c593473b3b02291a85f
Clarenth <standadit.dev@protonmail.com>
2024-02-02 13:52:13 -0500

    Task pushes overflowing text into ellipses.

--- frontend/src/routes/projects/_components/project/Task.svelte
+++ frontend/src/routes/projects/_components/project/Task.svelte
@@ -3,7 +3,7 @@
   import Edit from "$lib/images/edit-dark-svgrepo-com.svg"
 </script>
 
-<div class="flex flex-row gap-2 shadow-lg bg-[#303030] border border-black hover:border-white transition focus:outline-none focus:ring-2">
+<div class="flex flex-row gap-2 px-1 shadow-lg bg-[#303030] border border-black hover:cursor-pointer hover:border-white transition focus:outline-none focus:ring-2">
   <div class="flex flex-col gap-2 justify-between">
     <img 
       src={Edit}
@@ -16,10 +16,11 @@
       class="w-6 rounded-full mb-1 hover:bg-black transition-all"
     />
   </div>
-  <div class="flex flex-col justify-start items-start gap-1 min-w-72 max-w-72 text-wrap break-all">
-    <h3>Investigate qOwnNotes for self-hosting</h3>
-    <p class="text-sm">
-      Find out if qOwnNotes is a good notes app
+  <div class="flex flex-col justify-start items-start gap-1 min-w-72 max-w-72">
+    <h3 class="">Investigate qOwnNotes for self-hosting</h3>
+    <hr class="w-full opacity-50">
+    <p class="line-clamp-3 break-normal overflow-hidden">
+      Officially recognized by the Duden dictionary as the longest word in German, Kraftfahrzeughaftpflichtversicherung is a 36 letter word for motor vehicle liability insurance.
     </p>
     <!-- <p>Due Date</p> -->
   </div>
