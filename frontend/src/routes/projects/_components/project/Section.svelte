389a232fe6efc95ed700679e72cd68be97e72a63
Clarenth <standadit.dev@protonmail.com>
2024-02-06 15:59:10 -0500

    Cleaned up CSS for the section component.

--- frontend/src/routes/projects/_components/project/Section.svelte
+++ frontend/src/routes/projects/_components/project/Section.svelte
@@ -49,13 +49,13 @@ TODO
       autofocus
     />
     <div class="flex flex-row justify-end">
-    <Button type="submit" class="bg-red-700 rounded-none hover:bg-red-900" on:click={() => editSection = false}>
-      Cancel
-    </Button>
-    <Button type="submit" class="bg-[#3a3a3a] shadow-lg rounded-none focus:outline-none focus:ring-2">
-      Save
-    </Button>
-  </div>
+      <Button type="submit" class="bg-red-700 rounded-none hover:bg-red-900" on:click={() => editSection = false}>
+        Cancel
+      </Button>
+      <Button type="submit" class="bg-[#3a3a3a] shadow-lg rounded-none focus:outline-none focus:ring-2">
+        Save
+      </Button>
+    </div>
 {/if}
 
 <div class="space-y-0.5 overflow-y-scroll">
@@ -76,35 +76,34 @@ TODO
     Add Task
   </Button>
   {:else}
-  <form action="post" class="border border-none text-white">
-    <Input
-      id="title"
-      placeholder="Title" 
-      value={title}
-      autofocus
-      class="w-full bg-[#3a3a3a] text-base break-normal border-none"
-    />
-    <hr class="bg-white">
-    <Textarea
-      id="description"
-      placeholder="Description" 
-      value={description}
-      class="bg-[#3a3a3a] text-sm break-normal border-none "
-    />
-    <p>Due Date</p>
-  </form>
-    <Button type="submit" class="bg-red-700 hover:bg-red-900 rounded-none" on:click={() => addNewTask = false}>
-      Cancel
-    </Button>
-    <Button type="submit" class="w-full bg-[#3a3a3a] justify-center items-center shadow-lg rounded-t-none focus:outline-none focus:ring-2">
-      Save
-    </Button>
+    <form action="post" class=" border border-black text-white">
+      <Input
+        id="title"
+        placeholder="Title" 
+        value={title}
+        autofocus
+        class="w-full bg-[#3a3a3a] text-base break-normal border-none justify-center items-center text-center shadow-lg rounded-b-none focus:outline-none"
+      />
+      <hr class="bg-white">
+      <Textarea
+        id="description"
+        placeholder="Task Description" 
+        value={description}
+        class="h-44 bg-[#3a3a3a] text-base border-none focus:outline-none"
+      />
+      <!-- <p>Due Date</p> -->
+    </form>
+    <div class="flex flex-row justify-end">
+      <Button type="submit" class="bg-red-700 rounded-none hover:bg-red-900" on:click={() => addNewTask = false}>
+        Cancel
+      </Button>
+      <Button type="submit" class="bg-[#3a3a3a] shadow-lg rounded-none focus:outline-none focus:ring-2">
+        Save
+      </Button>
+    </div>
 {/if}
 
 <footer class="flex justify-center hover:visible">
-  
-
-
   <!-- <Button 
     on:click={() => (showModal = true)}
     class="w-full bg-[#3a3a3a] justify-center items-center shadow-lg rounded-t-none focus:outline-none focus:ring-2"
